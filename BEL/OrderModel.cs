aacade04d62f4b6ff1d445889badcc130b3ec030
Kaniz-Kanta <kanizkanta47@gmail.com>
2021-12-22 02:32:10 +0600

    Tier Application

---
+++ BEL/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEL
+{
+   public  class OrderModel
+    {
+        public int Id { get; set; }
+        public string OrderDate { get; set; }
+        public string OrderTime { get; set; }
+        public string P_Name { get; set; }
+    }
+}
