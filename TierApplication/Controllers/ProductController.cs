21d0f9e73f2185ac2fdd8d57a2aacb8fac263d61
Kaniz-Kanta <kanizkanta47@gmail.com>
2021-12-22 02:27:45 +0600

    All Done

---
+++ TierApplication/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using BEL;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TierApplication.Controllers
+{
+    public class ProductController : ApiController
+    {
+        [Route("api/product/All")]
+        [HttpGet]
+        public List<ProductModel> GetAll()
+        {
+            return ProductService.GetAll();
+        }
+        [Route("api/product/Names")]
+        [HttpGet]
+        public List<string> GetNames()
+        {
+            return ProductService.GetNames();
+        }
+    }
+}
