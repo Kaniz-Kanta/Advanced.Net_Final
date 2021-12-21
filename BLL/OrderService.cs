436871073f118e602f402abbb1ce1a95ba826ddd
Kaniz-Kanta <kanizkanta47@gmail.com>
2021-12-22 02:32:46 +0600

    Tier Application

---
+++ BLL/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using BEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DAL;
+
+namespace BLL
+{
+    class OrderService
+    {
+        public List<OrderModel> GetAllOrder()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Order, OrderModel>());
+            var mapper = config.CreateMapper();
+            var data = mapper.Map<List<OrderModel>>(DataAccessFactory.OrderDataAccess().Get());
+            return data;
+        }
+    }
+}
---
+++ BLL/ProductService.cs
@@ -0,0 +1,37 @@
+﻿using BEL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace BLL
+{
+    public class ProductService
+    {
+        public static List<ProductModel> GetAll()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductModel>());
+            var mapper = config.CreateMapper();
+            var data = mapper.Map<List<ProductModel>>(DataAccessFactory.ProductDataAccess().Get());
+            return data;
+        }
+        public static List<string> GetNames()
+        {
+            var data = (from e in DataAccessFactory.ProductDataAccess().Get()
+                        select e.ProductName).ToList();
+            //data = ProductRepo.GetAll().Select(e => e.ProductName).ToList();
+            return data;
+        }
+        public static void Edit()
+        {
+
+        }
+        public static void Delete()
+        {
+
+        }
+    }
+}
